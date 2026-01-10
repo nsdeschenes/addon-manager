31fcbbd4e69fce18f3a1ed8b6536c57f5a50edec
Nick <nsdeschenes@outlook.com>
2026-01-10 08:11:51 -0400

    feat: Load addon data (#4)
    * Add zod
    
    * Add zod schemas for content history and manifest files
    
    * Tweak addon and addon item types
    
    * Update load addons to load data from content history and manifest data

---
+++ src/schema.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+export const ContentHistorySchema = z.object({
+  "package-name": z.string(),
+  items: z.array(
+    z.object({
+      type: z.string(),
+      content: z.string(),
+      revision: z.number(),
+    })
+  ),
+});
+
+export const ManifestSchema = z.object({
+  dependencies: z.array(z.string()),
+  content_type: z.string(),
+  title: z.string(),
+  manufacturer: z.string(),
+  creator: z.string(),
+  package_version: z.string(),
+  minimum_game_version: z.string(),
+  release_notes: z.object({
+    neutral: z.object({
+      LastUpdate: z.string(),
+      OlderHistory: z.string(),
+    }),
+  }),
+});
