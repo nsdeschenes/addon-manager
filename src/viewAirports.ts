586d3db075f8b5b996ae407a81912c6eda643301
nsdeschenes <nsdeschenes@outlook.com>
2026-01-10 09:52:08 -0400

    Add viewAirports functionality

---
+++ src/viewAirports.ts
@@ -0,0 +1,27 @@
+import { autocomplete, box } from "@clack/prompts";
+import type { Addon } from "./types";
+import { renderAddon } from "./utils/renderAddon";
+
+export async function viewAirports(addons: Addon[]) {
+  const airports = addons.flatMap((addon) =>
+    addon.items
+      .filter((item) => /airport/i.test(item.type))
+      .map((item) => item.content)
+  );
+
+  const airport = await autocomplete({
+    message: "Select airports to view",
+    options: airports.map((airport) => ({
+      value: airport,
+      label: airport,
+    })),
+    maxItems: 10,
+  });
+
+  const selectedAddon = addons.find((a) =>
+    a.items.some((item) => item.content === airport)
+  )!;
+
+  const title = `${selectedAddon.title} - ${selectedAddon.packageName}`;
+  box(renderAddon(selectedAddon), title);
+}
